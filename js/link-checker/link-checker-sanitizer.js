309ccd3c7961990f8be9bc4855ff78ad31120528
Borja Molina Zea <borja.mz8@gmail.com>
2022-11-17 12:04:48 +0100

    changed patterns

--- js/link-checker/link-checker-sanitizer.js
+++ js/link-checker/link-checker-sanitizer.js
@@ -1,12 +1,19 @@
 const patrones = [
-    '.*/directorio-personal/.*',
-    '.*/docencia/grados/.*',
-    '.*/docencia/posgrados/.*',
+    '.*/docencia/grados/.*/guia-docente.*',
+    '.*/docencia/posgrados/.*/guia-docente.*',
 ]
-document.getElementsByTagName('table').array.forEach(elem => {
-    const url = elem[0].childNodes[1].childNodes[4].childNodes[1].textContent.split(',')[0]
+
+Array.from(document.getElementsByTagName('br')).forEach(elem => {
+    elem.style.display = "none"
+})
+
+Array.from(document.getElementsByTagName('table')).forEach(elem => {
+    const url = elem.childNodes[1].childNodes[4].childNodes[1].textContent.split(',')[0]
     let valido = patrones.every(x=>!url.match(x))
     if(!valido){
         elem.style.display = "none"
+    }
+    else{
+        elem.style.marginBottom = "3rem"
     }
 })
